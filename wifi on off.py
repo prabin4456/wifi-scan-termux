08af2cc1d722f1437444f99a2b57153a999e7469
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-28 23:29:23 +0545

    Add files via upload

---
+++ wifi on off.py
@@ -0,0 +1,34 @@
+'''Termux-API WiFi methods
+
+    on       - Enable Wifi
+    off      - Disable Wifi
+    info     - Connection information (JSON)
+    scaninfo - Last scan info (JSON)
+'''
+from .android import execute
+
+def on():
+    '''
+    Enable WiFi
+    '''
+    return execute("termux-wifi-enable true")
+
+def off():
+    '''
+    Disable WiFi
+    '''
+    return execute("termux-wifi-enable false")
+    
+
+
+def info():
+    '''
+    Return wifi connection info (json format)
+    '''
+    return execute(f"termux-wifi-connectioninfo")
+
+def scaninfo():
+    '''
+    Return last wifi scan info (json format)
+    '''
+    return execute(f"termux-wifi-scaninfo")
